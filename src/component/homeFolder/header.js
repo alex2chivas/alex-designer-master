ef9ef4dbb85f0567adb777eb92d51c836fffdac9
alex2chivas <alex2chivas@hotmail.com>
2020-07-05 03:28:19 -0700

    changed ig link http

--- src/component/homeFolder/header.js
+++ src/component/homeFolder/header.js
@@ -8,50 +8,46 @@ import OverlayDisplay from '../react-bootstrap/overlay';
 
 const Header = () => {
 	return (
-		<Container fluid className='header-navigation'>
-			<Row justify='center' className='header-content-wrapper'>
+		<Container fluid className="header-navigation">
+			<Row justify="center" className="header-content-wrapper">
 				<Col style={{ marginRight: '15px' }}>
 					<GoogleAuth />
 				</Col>
 				<Row>
 					<Col>
-						<OverlayDisplay type='HOME'>
-							<Link to='/' className='link'>
+						<OverlayDisplay type="HOME">
+							<Link to="/" className="link">
 								Home
 							</Link>
 						</OverlayDisplay>
 					</Col>
 					<Col>
-						<OverlayDisplay type='ABOUT'>
-							<Link to='/design/about' className='link'>
+						<OverlayDisplay type="ABOUT">
+							<Link to="/design/about" className="link">
 								About
 							</Link>
 						</OverlayDisplay>
 					</Col>
 					<Col>
-						<OverlayDisplay type='CONTACT'>
-							<Link to='/design/contact' className='link'>
+						<OverlayDisplay type="CONTACT">
+							<Link to="/design/contact" className="link">
 								Contact
 							</Link>
 						</OverlayDisplay>
 					</Col>
 					<Col>
-						<OverlayDisplay type='COLLECTIONS'>
-							<Link to='/design/collection' className='link'>
+						<OverlayDisplay type="COLLECTIONS">
+							<Link to="/design/collection" className="link">
 								Collection
 							</Link>
 						</OverlayDisplay>
 					</Col>
 				</Row>
 
-				<Col className='ig-wrapper'>
-					<OverlayDisplay type='INSTAGRAM'>
-						<a
-							target='_blank'
-							href='https://www.instagram.com/_beautesombre_/'
-							rel='noopener noreferrer'
-						>
-							<FaInstagram className='instagram' />
+				<Col className="ig-wrapper">
+					<OverlayDisplay type="INSTAGRAM">
+						<a target="_blank" href="https://www.instagram.com/beautesombre28/" rel="noopener noreferrer">
+							<FaInstagram className="instagram" />
 						</a>
 					</OverlayDisplay>
 				</Col>
