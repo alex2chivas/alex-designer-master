38ed47a7cb25b4cd99e8c4c39b5d7f117eefcc34
alex2chivas <alex2chivas@hotmail.com>
2020-07-05 12:17:14 -0700

    added dist folder

---
+++ dist/precache-manifest.a1c638395db0bd257b61ec62ae9b1941.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362667e0c00221088e792dd840bd32fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "044bfea66f15e3c3a111",
+    "url": "/static/css/main.0ff55a03.chunk.css"
+  },
+  {
+    "revision": "762336bd1e6471e87a20",
+    "url": "/static/js/2.16130c24.chunk.js"
+  },
+  {
+    "revision": "cb1d5545940dfee20c8a392560fedce7",
+    "url": "/static/js/2.16130c24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "044bfea66f15e3c3a111",
+    "url": "/static/js/main.894e1725.chunk.js"
+  },
+  {
+    "revision": "1d65582add0fdc657188",
+    "url": "/static/js/runtime-main.34397a34.js"
+  },
+  {
+    "revision": "f29bb801e350e71d820c6145154afcf5",
+    "url": "/static/media/about-image.f29bb801.jpg"
+  },
+  {
+    "revision": "ac6c1da645a803536915815528943345",
+    "url": "/static/media/building.ac6c1da6.jpg"
+  },
+  {
+    "revision": "584f15b14fefa4457fe9df1ad5de6f76",
+    "url": "/static/media/dark-image-one.584f15b1.jpeg"
+  },
+  {
+    "revision": "1a206d7da2105acdd303986f4c51a115",
+    "url": "/static/media/los-angeles.1a206d7d.jpg"
+  }
+]);
